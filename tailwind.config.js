08f7988968cf79f2dbaf99053244d7b20cf38f39
Jose-leandro <lenadrojose124leandro@gmail.com>
2024-04-23 09:48:56 -0300

    feat: Iniciando o desenvolvimento da nova versão 0.0.2 do meu portfólio
    Descrição:
    Para esta nova versão, pretendo implementar novos recursos, aprimorar o design, adicionar minhas tecnologias favoritas e muito mais.

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  }, netflix:///app?source_type=!
+   
+  plugins: [],
+}
+
